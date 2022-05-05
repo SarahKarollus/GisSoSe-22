f69505b5d0cdbc96014bb974f686162e10649285
SarahKarollus <92145369+SarahKarollus@users.noreply.github.com>
2022-05-05 14:39:05 +0200

    versuch

---
+++ versuch3/scrypt.js
@@ -0,0 +1,14 @@
+/* Funktionalität MUST */
+
+
+this.document.getElementById('SubmitCategorie').style.display = 'none';
+var n = 1.1;
+this.document.getElementById('DS').value = n.toLocaleString().substring(1, 2);
+
+	
+
+
+
+
+
+
